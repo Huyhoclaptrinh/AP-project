a9f0e09ff1e2c85f246c54db817874c783766ef4
huypg <giahuyphung282@gmail.com>
2022-04-22 15:25:55 +0700

    Luot cua Khoa-Nam

--- main.py
+++ main.py
@@ -4,35 +4,46 @@ import sqlite3
 
 window=Tk()
 window.geometry("1000x900")
+# Phan cua Khoa:
+# Phan dang nhap
 def user_win():
     user = Tk()
     user.geometry("300x300")
+    # Dang nhap xong, xac nhan va bam nut ben duoi de chuyen sang phan menu cua User
     bt_user_signin = Button(user,text="Sign in",width=20,command=user_menu_win).pack(side=LEFT)
+    # Nut dang ky, bam vao thi se chuyen sang tab khac de dien thong tin dang ky
+    # Khoa tim cach de luu thong tin dang ky va lien ket voi phan dang nhap de xac nhan tai khoan dang nhap
     bt_user_signup = Button(user,text="Sign up",width=20,command=user_signup_win).pack(side=RIGHT)
 
 def user_menu_win():
     user_menu = Tk()
     user_menu.geometry("300x300")
-    
+# Thiet ke giao dien cho phan dang ky
 def user_signup_win():
     user_signup = Tk()
     user_signup.geometry("300x300")
-    
+# Thiet ke giao dien cho phan dang nhap Admin(Khoa de co dinh 1 tk thi tot, k thi cung ksao)   
 def admin_win():
     admin = Tk()
     admin.geometry("300x300")
     bt_admin_signin = Button(admin,text="Sign in",width=20,command=admin_menu_win).pack()
-
+# Phan cua Nam:
 def admin_menu_win():
     admin_menu = Tk()
     admin_menu.geometry("300x300")
+    # Thay list thanh table
     def addlist():
         listbox.insert(END, entry.get())
         entry.delete(0, END)
 
     def removelist():
         listbox.delete(listbox.curselection())
-    
+    # Day la phan giao dien de dien thong tin de them vao bang
+    def add_win():
+        add_win_menu = Tk()
+        add_win_menu.geometry("300x300")
+        # Nut submit de add thong tin vao table
+        bt_submit_add = Button(add_win_menu,width = 20, command=addlist).pack()
     
     listbox = Listbox(admin_menu)
     listbox.pack()
@@ -40,11 +51,11 @@ def admin_menu_win():
     entry = Entry(admin_menu)
     entry.pack()
     
-    button = Button(admin_menu, text="Add book",command=addlist)
-    button.pack()
+    bt_add = Button(admin_menu, text="Add book",command=add_win)
+    bt_add.pack()
     
-    button = Button(admin_menu, text="Remove book",command=removelist)
-    button.pack()
+    bt_remove = Button(admin_menu, text="Remove book",command=removelist)
+    bt_remove.pack()
 
 l1=Label(window,text="BOOKSTORE MANAGEMENT",font="times 20")
 l1.grid(row=1,column=2,columnspan=2)
