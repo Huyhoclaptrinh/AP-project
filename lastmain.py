832c12bf3412c808d8a3163b52b958e56c906e02
huypg <giahuyphung282@gmail.com>
2022-04-29 11:24:37 +0700

    Phan code giao dien menu vua bi lap nen t xoa, con lai van on

--- lastmain.py
+++ lastmain.py
@@ -453,13 +453,4 @@ bt_admin.grid(row=3,column=2)
 bt_user=Button(window,text="USER",width=20,command=user_win)
 bt_user.grid(row=3,column=3)
 
-l1=Label(window,text="BOOKSTORE MANAGEMENT",font="times 20")
-l1.grid(row=1,column=2,columnspan=2)
-
-bt_admin=Button(window,text="ADMIN",width=20,command=admin_win)
-bt_admin.grid(row=3,column=2)
-
-bt_user=Button(window,text="USER",width=20,command=user_win)
-bt_user.grid(row=3,column=3)
-
 window.mainloop() 
